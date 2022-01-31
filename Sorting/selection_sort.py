e60ed0ea2fb3085df80e85513adbd5d1b36b5cca
karanS08 <66710785+karanS08@users.noreply.github.com>
2022-01-31 11:15:24 +0530

    Create selection_sort.py

---
+++ Sorting/selection_sort.py
@@ -0,0 +1,12 @@
+import sys
+A = [9,55,43,46,11,0,75]
+for i in range(len(A)):
+	min_idx = i
+	for j in range(i+1, len(A)):
+		if A[min_idx] > A[j]:
+			min_idx = j
+	A[i], A[min_idx] = A[min_idx], A[i]
+  
+print ("Sorted array")
+for i in range(len(A)):
+	print("%d" %A[i]),
